cd0ae004daf68fafddce40b6de7caf0bb6454818
Arda Dere <ardadere114@gmail.com>
2025-03-07 21:02:48 +0300

    Proje dosyası ekle.

---
+++ Hesaplama/Hesaplama.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik
+{
+    public class Hesaplama
+    {
+        public int topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+}
---
+++ Hesaplama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Matematik; 
+
+namespace ProgramNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Birinci sayıyı girin: ");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin: ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+
+            Hesaplama hesap = new Hesaplama();
+
+
+            int toplamSonuc = hesap.topla(sayi1, sayi2);
+            int carpimSonuc = hesap.carp(sayi1, sayi2);
+
+
+            Console.WriteLine($"Toplam: {toplamSonuc}");
+            Console.WriteLine($"Çarpım: {carpimSonuc}");
+
+            Console.ReadKey();
+        }
+    }
+}
